a11f9c9f2eee7c43099ee339cbe83c0e29ec3c56
hp <vikashkr.y2020@outlook.com>
2023-05-23 15:20:45 +0530

    code

---
+++ github-cicd-actions/src/test/java/com/javatechi/cicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechi.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
